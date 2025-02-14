7b9d0f388608a7156833864eb5d8be80b6973b63
BeruzDev <135733187+BeruzDev@users.noreply.github.com>
2025-02-14 16:45:40 +0100

    Clase 3 done!

--- clase-3_CORS_API_REST/app.js
+++ clase-3_CORS_API_REST/app.js
@@ -1,17 +1,50 @@
 const express = require('express')
 const crypto = require('node:crypto')
+const cors = require('cors')
 const { validateMovie } = require('./schemas/movies')
 const { validationPartialMovie } = require('./schemas/movies')
 const movies = require('./movies.json')
 
 const app = express()
 app.use(express.json())
+app.use(cors({
+  origin: (origin, callback) => {
+    //Podemos tener los puertos y dominios que queremos que acepte nuestra API para prevenir errores CORS
+    const ACCEPTED_ORIGINS = [
+      'http://localhost:8080',
+      'http://localhost:1234',
+      'http://movies.com',
+      'http://midu.dev',
+    ]
+
+    if (ACCEPTED_ORIGINS.includes(origin) || !origin){
+      return callback(null, true)
+    }
+
+    return callback(new Error('Not allowed by CORS'))
+  }
+}))
 app.disable('x-powered-by')
+
+//Métodos normales --> GET/HEAD/POST
+//Métodos complejos --> PUT/PATCH/DELETE
+
+// CORS PRE-Flight -> petición especial para métodos complejos --> Options
+// OPTIONS
 
 /*---------------------------------------------------------------------------------------*/
 
-// GET // Los recursos que sean MOVIES se identifican como /movies 
+
+//Si la petición es de localhost:1234 intentando acceder a localhost:1234 el navegador no envía el 'origin'
+
+// GET // Los recursos que sean MOVIES se identifican como /movies
 app.get('/movies', (req, res) => {
+  //'*' acepta cualquier origen, pero si queremos tenerlos controlados:
+  /*const origin = req.header('origin') //<--Guardamos el origen des del que se intenta acceder y que viene en la petición
+  if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+    //<-- si el origen de la cabecera se incluye en los orígenes aceptados o si no hay origen
+    res.header('Access-Control-Allow-Origin', origin) //! <-- IMPORTANTE ERROR CORS--> todos los orígenes que no sean el nuestro están permitidos "error CORS"
+  }*/
   const { genre, director, year } = req.query
 
   if (genre) {
@@ -42,7 +75,7 @@ app.get('/movies', (req, res) => {
 // Recuperar por id
 app.get('/movies/:id', (req, res) => {
   // path-to-regexp
-  const { id } = req.params//-> De los parámetros que hay en el body recoger el ID 
+  const { id } = req.params //-> De los parámetros que hay en el body recoger el ID
   const movie = movies.find((movie) => movie.id === id)
   if (movie) return res.json(movie)
   res.status(404).json({ message: 'Movie not found' })
@@ -77,22 +110,21 @@ app.post('/movies', (req, res) => {
 // PATCH // Actualizar parcialmente un elemento
 app.patch('/movies/:id', (req, res) => {
   const { id } = req.params
-  const movieIndex = movies.findIndex(movie => movie.id === id)
+  const movieIndex = movies.findIndex((movie) => movie.id === id)
 
-  if (movieIndex === -1){
+  if (movieIndex === -1) {
     return res.status(404).json({ message: 'Movie not found' })
   }
 
   const result = validationPartialMovie(req.body)
-  
-  if(!result.success){
+
+  if (!result.success) {
     return res.status(404).json({ error: JSON.parse(result.error.message) })
   }
-  
 
   const updateMovie = {
     ...movies[movieIndex],
-    ...result.data
+    ...result.data,
   }
 
   movies[movieIndex] = updateMovie
@@ -103,6 +135,31 @@ app.patch('/movies/:id', (req, res) => {
 /*---------------------------------------------------------------------------------------*/
 
 // PUT // Actualizar totalmente un elemento ya existente o crearlo si no existe
+
+/*---------------------------------------------------------------------------------------*/
+
+app.delete('/movies/:id', (req, res) => {
+  const { id } = req.params
+  const movieIndex = movies.findIndex((movie) => movie.id === id)
+
+  if (movieIndex === -1) {
+    return res.status(400).json({ message: 'Movie not found' })
+  }
+
+  movies.splice(movieIndex, 1)
+
+  return res.json({ message: 'Movie deleted' })
+})
+
+//Para métodos complejos añadir cabecera OPTIONS
+// app.options('/movies/:id', (req, res) => {
+//   const origin = req.header('origin')
+//   if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+//     res.header('Access-Control-Allow-Origin', origin) // hay que indicar el origen
+//     res.header('Access-Control-Allow-Methods', 'GET', 'POST', 'PATCH', 'DELETE') //y ademas hay que indicar el método permitido
+//   }
+//   res.send(200)
+// })
 
 const PORT = process.env.PORT ?? 1234
 
