02f9bcfbcf38a971daf7e441e927f3e702177979
BeruzDev <135733187+BeruzDev@users.noreply.github.com>
2025-03-07 18:36:54 +0100

    Primeros pasos

---
+++ clase-7_User_Auth/index.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+
+const PORT = process.env.PORT ?? 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello Beruz!!')
+})
+
+app.post('/login', (req, res) => {})
+app.post('/register', (req, res) => {})
+app.post('/logout', (req, res) => {})
+app.get('/protected', (req, res) => {})
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
